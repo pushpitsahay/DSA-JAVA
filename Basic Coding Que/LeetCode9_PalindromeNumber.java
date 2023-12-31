f96d0a2613d30723b5d01eba001e9732344979cc
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-01 00:10:03 +0530

    Add files via upload

---
+++ Basic Coding Que/LeetCode9_PalindromeNumber.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/palindrome-number/
+class Solution{
+    public boolean isPalindrome(int x){
+        int rev=0;
+        int temp=x;
+        if(x<0) return false;
+        while(x!=0){
+            rev = rev*10 + x%10;
+            x=x/10;
+        }
+        return rev==temp;
+    }
+}
