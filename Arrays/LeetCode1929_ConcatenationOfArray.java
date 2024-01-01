3267baa3a7bf9f58418251751a44cb7a08f92221
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-01 22:02:26 +0530

    Create LeetCode1929_ConcatenationOfArray.java

---
+++ Arrays/LeetCode1929_ConcatenationOfArray.java
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/concatenation-of-array/
+class Solution{
+    public int[] getConcatenation(int[] nums){
+        int ans[]=new int[2*nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i+nums.length]=ans[i]=nums[i];
+        }
+        return ans;
+    }
+}
