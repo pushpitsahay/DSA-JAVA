f2f8a72b531d0c50d919b67291037f05ac9c1fb1
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-04 00:47:56 +0530

    Create LeetCode1281_SubtractTheProductAndSumOfDigitsOfAnInteger.java

---
+++ Math/LeetCode1281_SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+class Solution{
+    public int subtractProductAndSum(int n){
+        int product=1;
+        int sum=0;
+        while(n>0){
+            product=product*(n%10);
+            sum=sum+(n%10);
+            n=n/10;
+        }
+        return product-sum;
+    }
+}
