f2a872e2f3a153203e90190da071dfc4a5a782bb
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-04 20:35:34 +0530

    Create LeetCode2469_ConvertTheTemperature.java

---
+++ Math/LeetCode2469_ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/convert-the-temperature/
+class Solution{
+    public double[] convertTemperature(double celsius){
+        return new double[] {celsius+273.15,(celsius*1.8)+32};
+    }
+}
