4f623ff3f1470ec4a54aaa06490129b1498c3cf5
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-10 16:43:04 +0530

    Add files via upload

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,22 @@
+// https://www.geeksforgeeks.org/problems/binary-search-1587115620/1?page=1&category=Searching&status=solved&sortBy=submissions
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int low=0;
+        int high=arr.length-1;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(k>arr[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
