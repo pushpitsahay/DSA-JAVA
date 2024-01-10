de54e112a6bd706865fe9a733e6af09a2cb62118
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-10 17:04:57 +0530

    Create LeetCode125_ValidPalindrome.java

---
+++ String/LeetCode125_ValidPalindrome.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/valid-palindrome/
+class Solution{
+    public boolean isPalindrome(String s){
+        // Convert the string to lowercase and remove non-alphanumeric characters
+        s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        
+        int left=0, right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
