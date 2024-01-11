ff1b2d1a45879cd4b72ed877b1239f5dd63277ca
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-11 23:26:55 +0530

    Create LeetCode1920_BuildArrayFromPermutation.java

---
+++ Arrays/LeetCode1920_BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+class Solution{
+    public int[] buildArray(int[] nums){
+        int n=nums.length;
+        for(int i=0; i<n; i++){
+            nums[i]+=(nums[nums[i]]%n)*n;
+        }
+        for(int i=0; i<n; i++){
+            nums[i]/=n;
+        }
+        return nums;
+    }
+}
