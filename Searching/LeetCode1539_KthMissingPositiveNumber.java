3ce5585d21ac36d6264e066ad7929082ef8e84c0
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-13 22:19:10 +0530

    Create LeetCode1539_KthMissingPositiveNumber.java

---
+++ Searching/LeetCode1539_KthMissingPositiveNumber.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/kth-missing-positive-number/
+class Solution{
+    public int findKthPositive(int[] arr, int k){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<=k){
+                k++;
+            }
+            else break;
+        }
+        return k;
+    }
+}
