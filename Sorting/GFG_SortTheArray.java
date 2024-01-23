896608781fb302ebfd286df1a1c722330f6c6900
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-01-24 01:13:22 +0530

    Create GFG_SortTheArray.java

---
+++ Sorting/GFG_SortTheArray.java
@@ -0,0 +1,8 @@
+// https://www.geeksforgeeks.org/problems/sort-the-array0055/1?page=1&category=Sorting&status=solved&sortBy=submissions
+class Solution{ 
+    int[] sortArr(int[] arr, int n){ 
+        // code here
+        Arrays.sort(arr);
+        return arr;
+    }
+} 
