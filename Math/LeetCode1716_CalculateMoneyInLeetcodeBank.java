0f3b674c2d98c6ea39edf3249468d74b360e8f12
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-05-02 01:36:24 +0530

    Create LeetCode1716_CalculateMoneyInLeetcodeBank.java

---
+++ Math/LeetCode1716_CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+class Solution{
+    public int totalMoney(int n){
+        int ans=0;
+        for(int i=0; i<n; i++){
+            ans+=(i/7)+1+(i%7);
+        }
+        return ans;
+    }
+}
